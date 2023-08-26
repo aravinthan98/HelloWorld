1bb96b1f39a5b7eb2d1583930293c380794ab4d5
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2023-08-26 12:53:23 +0530

    newfile

--- sorting/bubblesort.java
+++ sorting/bubblesort.java
@@ -7,22 +7,21 @@ public class bubblesort {
         int[] arr = {5, 4, 1, 6, 9, 7, 3};//1 3 4 5 6 7 9
         int n = arr.length;
         boolean swapped;
-        for(int i = 0; i < n-1; i++) {
-            swapped=false;
-            for(int j = 0; j < n- 1-i; j++) {
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     swap(arr, j, j + 1);
-                    swapped=true;
+                    swapped = true;
                 }
             }
-            if(!swapped){
+            if (!swapped) {
                 break;
             }
         }
 
         util.printArrays(arr);
     }
-
     public static void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
